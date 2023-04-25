8d976f95f0a9a24473fca5abdc749a14acb8c829
ARMorCoder <91237516+ARMorCoder@users.noreply.github.com>
2023-04-25 16:52:04 -0500

    updated chatgui (calmer looking chat box)

--- chat/chatgui.py
+++ chat/chatgui.py
@@ -127,7 +127,7 @@ base.geometry("400x500")
 base.resizable(width=FALSE, height=FALSE)
 
 #Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog = Text(base, bd=0, bg="#8ecfd1", height="8", width="50", font="Arial",)
 
 ChatLog.config(state=DISABLED)
 
@@ -136,12 +136,12 @@ scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
 ChatLog['yscrollcommand'] = scrollbar.set
 
 #Create Button to send message
-SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+SendButton = Button(base, font=("Lucida Console",12,'bold'), text="Send", width="10", height=5,
+                    bd=0, bg="#2c5e9e", activebackground="#3c9d9b",fg='#ffffff',
                     command= send )
 
 #Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox = Text(base, bd=0, bg="#47d69f",width="29", height="5", font="Arial")
 #EntryBox.bind("<Return>", send)
 
 
@@ -149,7 +149,7 @@ EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
 scrollbar.place(x=376,y=6, height=386)
 ChatLog.place(x=6,y=6, height=386, width=370)
 EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+SendButton.place(x=5, y=400, height=90)
 
 base.mainloop()
 
